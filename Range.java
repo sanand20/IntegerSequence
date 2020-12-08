383ec9971be4fe81be95550d26918d8b6b69e970
Shriya <>
2020-12-07 19:38:44 -0500

    range done

---
+++ Range.java
@@ -0,0 +1,40 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+  private int start,end,current;
+
+  /*
+*@precondition: start <= end
+*@param start : the starting value (inclusive)
+*@param end : the ending value which is also inclusive.*/
+  public Range(int start,  int end){
+  if (start>end){
+    {throw new IllegalArgumentException("start is greater than end");
+  }
+  this.start = start;
+  this.end = end;
+  current = start;
+  }
+}
+
+  public void reset(){  }
+  public int length(){  }
+
+  //When current is no longer a valid element in the range, it should return false.
+  public boolean hasNext(){  }
+
+  //@throws NoSuchElementException when hasNext() is false.
+  //This will return the current value, it will also increase current value by 1.
+  //e.g.  if current is 5. This will make current 6, and return 5.
+  public int next(){    }
+
+}
+
+
+    IntegerSequence r = new Range(10,15);
+    while(r.hasNext()){
+      System.out.print(r.next())
+      if( r.hasNext() ){
+         System.out.print( ", " );
+      }
+    }
+    System.out.println();
