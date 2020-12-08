d4ad9a5bb6a36a2d83b59ad480f6e2429a95e866
Shriya <>
2020-12-08 12:50:48 -0500

    added constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,24 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+  for (int i = 0; i<other.length-1; i++){
+    data[i]=other[i];
+  }
+  currentIndex=0;}
+  public void reset(){
+  currentIndex = 0; }
+  public boolean hasNext(){
+    if (currentIndex<data.length){
+      return true;
+    }
+    return false;
+  }
+  public int next(){
+    if (hasNext()==false){
+      throw new  NoSuchElementException("error");
+    }
+    currentIndex++;
+    return (currentIndex - 1);
+ }
 }
